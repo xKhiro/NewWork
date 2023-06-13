5b3e42a1a1ad9a3d3978ce5fb39dde3b8cc9f940
xKhiro <louis.markelstorfer@gmx.de>
2023-06-13 20:26:19 +0200

    add cors header

--- Backend/src/cmd/bookingsystem/main.go
+++ Backend/src/cmd/bookingsystem/main.go
@@ -13,6 +13,8 @@ func main() {
 	log.Println("Start server")
 	router := mux.NewRouter()
 
+	router.Use(corsMiddleware)
+
 	myRouter := routers.NewRouter()
 
 	router.HandleFunc("/workspaces", myRouter.GetWorkspaces).Methods("GET")
@@ -21,4 +23,22 @@ func main() {
 	router.HandleFunc("/users/{personId}/bookings/{bookingId}", myRouter.CancelBooking).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Setze die erforderlichen CORS-Header
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Wenn der Preflight-Request (OPTIONS) gesendet wird, beantworte ihn sofort
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Rufe den nächsten Handler auf
+		next.ServeHTTP(w, r)
+	})
 }
